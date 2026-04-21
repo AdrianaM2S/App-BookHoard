a47cd4e5b7a4a4858cb3b377fc54fcb7267293ed
AdrianaM2S <adriana.m.marrero16@gmail.com>
2026-04-21 12:34:31 -0400

    Database update

--- database/data.sql
+++ database/data.sql
@@ -16,17 +16,17 @@ CREATE TABLE user (
     FOREIGN KEY (role_id) REFERENCES role(id)
 );
 
-CREATE TABLE book (
+CREATE TABLE books (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(150),
     author VARCHAR(100),
-    synopsys VARCHAR(500),
-    cover BLOB,
+    synopsis VARCHAR(250),
+    cover_data MEDIUMBLOB,
     type_book VARCHAR(30),
     category VARCHAR(50),
-    isbn VARCHAR(20), 
+    isbn VARCHAR(20) UNIQUE, 
     publish_date DATE,
-    languege VARCHAR(30),
+    languege VARCHAR(30)
 );
 
 INSERT INTO role (name) VALUES ('Admin'), ('User');
